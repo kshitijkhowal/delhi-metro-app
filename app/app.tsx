86423e8c21165b9e4e1efcf5abda913e72aee85d
Kshitij Khowal <kshitij.durgesh@gmail.com>
2025-07-13 19:53:23 +0530

    chore: added a react navigation instead of expo navigation

--- app/app.tsx
+++ app/app.tsx
@@ -1,19 +1,26 @@
-import { Stack } from 'expo-router';
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store/index'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from './screens/SplashScreen/SplashScreen';
+// import other screens as needed
 
-export default function RootLayout() {
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-      <Stack.Screen name="index" />
-
-    </Stack>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="SplashScreen">
+          <Stack.Screen
+            name="SplashScreen"
+            component={SplashScreen}
+            options={{ headerShown: false }}
+          />
+          {/* Add more screens here */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './app/app'; // or './app/index' if you prefer
+
+AppRegistry.registerComponent('main', () => App);
